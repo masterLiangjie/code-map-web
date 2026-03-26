61963ae4494563d2fd958aceb0a58ce857559e0c
林锋 <14367301+yuncun-foshan@user.noreply.gitee.com>
2026-03-26 18:24:30 +0800

    Update .gitignore to exclude 'dist/' directory, add custom font variables in globals.css, and clean up layout.tsx by removing unused font imports. Also, delete obsolete page.js file from dist directory.

--- app/dist/page.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function Home() {
-    return (React.createElement("div", { className: "flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black" },
-        React.createElement("h2", null, "\u6211\u7684\u7B2C\u4E00\u4E2A next \u9875\u9762")));
-}
-exports["default"] = Home;
